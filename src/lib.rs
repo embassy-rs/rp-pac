656179e4588885bb86a1f8e2f724e7a4e44e4064
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-08-11 21:58:17 +0200

    better errors if no or both features are enabled.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 #![allow(non_camel_case_types)]
 #![doc = include_str!("../README.md")]
 
-#[cfg(feature = "rp2040")]
-pub mod rp2040;
-#[cfg(feature = "rp2040")]
-pub use rp2040::*;
+#[cfg(all(feature = "rp2040", feature = "rp235x"))]
+compile_error!("You must not enable both the `rp2040` and `rp235x` Cargo features.");
+#[cfg(not(any(feature = "rp2040", feature = "rp235x")))]
+compile_error!("You must enable either the `rp2040` or the `rp235x` Cargo features.");
 
-#[cfg(feature = "rp235x")]
-pub mod rp2350;
-#[cfg(feature = "rp235x")]
-pub use rp2350::*;
+#[cfg_attr(feature = "rp2040", path = "./rp2040/mod.rs")]
+#[cfg_attr(feature = "rp235x", path = "./rp235x/mod.rs")]
+mod inner;
+pub use inner::*;
